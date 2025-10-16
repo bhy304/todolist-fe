add7be96d47a2d8cd3dd782c4cf2263ce9adc1b0
Hayeon Baek <bhy0512@gmail.com>
2025-10-16 16:31:58 +0900

    feat: react-router-dom을 사용한 페이지 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Todo from './pages/todo';
+import Login from './pages/login';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
